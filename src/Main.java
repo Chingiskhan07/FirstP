ce6a74b29097187bd4834cccf1ca5212786148f4
Шынгыс <254768@astanait.edu.kz>
2025-12-14 21:44:19 +0500

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-package Chingiskhan.parking;
+package Chingiskhan.library;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Chingiskhan and Welcome to the Library Management System!");
         System.out.println("This project is designed to manage books and users in a library.");
         System.out.println("Main features of the system:");
-        System.out.println("Add and remove books");
-        System.out.println("Register library users");
-        System.out.println("Track borrowed and returned books");;
+        System.out.println("-Add and remove books");
+        System.out.println("-Register library users");
+        System.out.println("-Track borrowed and returned books");;
     }
 }
