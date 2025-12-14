841b458b5d6a02cad106cb0e3ee25cee637841bd
Шынгыс <254768@astanait.edu.kz>
2025-12-14 21:44:19 +0500

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+package Chingiskhan.parking;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Chingiskhan and Welcome to the Library Management System!");
+        System.out.println("This project is designed to manage books and users in a library.");
+        System.out.println("Main features of the system:");
+        System.out.println("Add and remove books");
+        System.out.println("Register library users");
+        System.out.println("Track borrowed and returned books");;
     }
 }
